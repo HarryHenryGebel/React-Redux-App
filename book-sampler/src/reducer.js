7d8cfa0e0777f3e7be7d30967af9f391ce03b14c
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-13 16:20:11 -0400

    Create redux store

--- book-sampler/src/reducer.js
+++ book-sampler/src/reducer.js
@@ -1,2 +1,8 @@
-const goodreadsKey = process.env.REACT_APP_GOODREADS_KEY,
-      goodreadsSecret = process.env.REACT_APP_GOODREADS_SECRET;
+const initialState = {
+  goodreadsKey: process.env.REACT_APP_GOODREADS_KEY,
+  goodreadsSecret: process.env.REACT_APP_GOODREADS_SECRET
+};
+
+export default function reducer (state = initialState, action) {
+  return initialState;
+}

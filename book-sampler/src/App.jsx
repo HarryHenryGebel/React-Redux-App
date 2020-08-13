e369907532106a5b49e11256c1ec51c6b5c4d76d
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-13 17:42:10 -0400

    Create BookForm

--- book-sampler/src/App.jsx
+++ book-sampler/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import BookForm from "./components/BookForm";
+
 export default function App() {
-  debugger;
   return (
     <>
+      <BookForm/>
     </>
   );
 }
+
+//  LocalWords:  BookForm
---
+++ book-sampler/src/components/BookForm.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+
+const initialValues = {author: "", title: ""};
+
+export default function BookForm () {
+  const [values, setValues] = useState(initialValues);
+
+  return (
+    <></>
+  );
+}

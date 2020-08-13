4051c5f6ee8ec63bcb387d15bbaf06b77131530d
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-13 16:55:40 -0400

    transposed spreads

--- book-sampler/src/reducer.js
+++ book-sampler/src/reducer.js
@@ -8,13 +8,13 @@ const initialState = {
 export default function reducer (state = initialState, action) {
   switch(action.command) {
   case FETCH_BOOK:
-    return state;
+    return {...state,
+            isFetching: true,
+            error: null};
   default:
     // return state if it is called by redux, otherwise throw error
     if (action.type.includes("@@redux"))
-      return {...state,
-              isFetching: true,
-              error: null};
+      return state;
     else
       throw Error(`Invalid action "{$action.type}"`);
   }

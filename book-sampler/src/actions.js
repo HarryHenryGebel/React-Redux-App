4120e2ba145c7a5d85a25bd5ed63159d1ed3af17
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-13 17:27:12 -0400

    Flesh out FETCH_BOOK action

--- book-sampler/src/actions.js
+++ book-sampler/src/actions.js
@@ -1,5 +1,38 @@
+import requester from "easier-requests";
+
 export const FETCH_BOOK = "FETCH_BOOK";
 
-export function fetchBook (dispatch, title) {
+const goodreadsKey = process.env.REACT_APP_GOODREADS_KEY,
+      goodreadsSecret = process.env.REACT_APP_GOODREADS_SECRET;
+
+const lastRequestTime = 0;
+export function fetchBook (dispatch, title, author) {
+  async function _fetchBook() {
+    debugger;
+    try {
+      const id = requester.createUniqueID(),
+            params = {key: goodreadsKey, title: title};
+      if (author) // author is optional
+        params.author = author;
+
+      await requester.get("https://www.goodreads.com/book/title.json",
+                          id,
+                          params);
+      console.log(requester.response(id));
+    }
+    catch (error) {
+      console.log(error);
+      throw error;
+    }
+    finally {
+      lastRequestTime = Date.now();
+    }
+  }
+
   dispatch({command: FETCH_BOOK, title: title});
+
+  // per TOS, limit requests to 1 per second
+  const nextRequestTime = lastRequestTime + 1000,
+        timeUntilRequest = nextRequestTime - Date.now();
+  setTimeout(_fetchBook, timeUntilRequest);
 }

86e863141dbd297f3367276ee78547a20acdaff7
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-13 20:03:42 -0400

    Make into real functions, like Pinocchio

--- book-sampler/src/actions.js
+++ book-sampler/src/actions.js
@@ -6,34 +6,34 @@ const goodreadsKey = process.env.REACT_APP_GOODREADS_KEY;
 //      goodreadsSecret = process.env.REACT_APP_GOODREADS_SECRET;
 
 let lastRequestTime = 0;
-export function fetchBook (dispatch, title, author) {
-  async function _fetchBook() {
-    debugger;
-    try {
-      const id = requester.createUniqueID(),
-            params = {key: goodreadsKey, title: title};
-      if (author) // author is optional
-        params.author = author;
+export function fetchBook (title, author) {
+  return function (dispatch) {
+    async function _fetchBook() {
+      debugger;
+      try {
+        const id = requester.createUniqueID(),
+              params = {key: goodreadsKey, title: title};
+        if (author) // author is optional
+          params.author = author;
 
-      await requester.get("https://www.goodreads.com/book/title.json",
-                          id,
-                          params);
-      console.log(requester.response(id));
+        await requester.get("https://www.goodreads.com/book/title.json",
+                            id,
+                            params);
+        console.log(requester.response(id));
+      }
+      catch (error) {
+        console.log(error);
+        throw error;
+      }
+      finally {
+        lastRequestTime = Date.now();
+      }
     }
-    catch (error) {
-      console.log(error);
-      throw error;
-    }
-    finally {
-      lastRequestTime = Date.now();
-    }
-  }
+    dispatch({type: FETCH_BOOK, title: title});
 
-  dispatch({command: FETCH_BOOK, title: title});
-  dispatch({type: FETCH_BOOK, title: title});
-
-  // per TOS, limit requests to 1 per second
-  const nextRequestTime = lastRequestTime + 1000,
-        timeUntilRequest = nextRequestTime - Date.now();
-  setTimeout(_fetchBook, timeUntilRequest);
+    // per TOS, limit requests to 1 per second
+    const nextRequestTime = lastRequestTime + 1000,
+          timeUntilRequest = nextRequestTime - Date.now();
+    setTimeout(_fetchBook, timeUntilRequest);
+  };
 }
